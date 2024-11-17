5fe21a9935cc53e420e3ce727aeb0ce376643276
Ilshat Khaziev <haziev.ilshat92@gmail.com>
2024-11-17 23:27:21 +0300

    Add SWRequester clas

--- swapi.py
+++ swapi.py
@@ -1,35 +1,60 @@
 import requests
-import os
+from pathlib import Path
 
 
 class APIRequester:
-    def __init__(self, base_url: str):
+    def __init__(self, base_url: str) -> None:
         self.base_url = base_url
 
+        if self.base_url.endswith('/'):
+            self.base_url = self.base_url.rstrip('/')
+
     def get(self, url=''):
+        if url.startswith('/'):
+            url = url.lstrip('/')
+
+        link = f'{self.base_url}/{url}'
         try:
-            response = requests.get(f'{self.base_url}{url}')
+            response = requests.get(link)
             response.raise_for_status()
             return response
         except requests.HTTPError:
-            print('Статус коды от 400 до 599')
+            print('Ошибка на стороне клиента или сервера')
         except requests.RequestException:
             print('Возникла ошибка при выполнении запроса')
 
 
 class SWRequester(APIRequester):
     def get_sw_categories(self) -> list:
-        url = 'https://swapi.dev/api/'
-        pass
+        response = self.get().json()
+        return response.keys()
 
-    def get_sw_info(self, sw_type) -> str:
-        url = f'https://swapi.dev/api/{sw_type}/'
-        pass
-
-    def save_sw_data(self):
-        pass
+    def get_sw_info(self, sw_type: str) -> str:
+        response = self.get(f'{sw_type}/')
+        return response.text
 
 
-api = APIRequester(base_url='https://swapi.dev/')
-response = api.get('api')
-print(response)
+def save_sw_data():
+    Path('data').mkdir(exist_ok=True)
+
+    sw_request = SWRequester('https://swapi.dev/api')
+    categories = sw_request.get_sw_categories()
+
+    for category in categories:
+        path = f'data/{category}.txt'
+        with open(path, 'w') as f:
+            category_data = sw_request.get_sw_info(category)
+            f.write(category_data)
+
+
+if __name__ == '__main__':
+    save_sw_data()
+
+
+# api = APIRequester(base_url='https://swapi.dev/api')
+# response = api.get()
+# #print(response.json())
+
+# sw_req = SWRequester(base_url='https://swapi.dev/api')
+# print(sw_req.get_sw_categories())
+# # print(sw_req.get_sw_info('people'))
